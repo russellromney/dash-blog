ebff72fbe8e5fffb02a1fb5e43be15092f705fcd
russellromney <russdude7@gmail.com>
2019-06-18 13:12:08 -0500

    added function

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import dash
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+from dash import no_update
 
 
 app = dash.Dash(
@@ -13,3 +17,30 @@ app = dash.Dash(
 app.title = 'Russell Romney'
 app.config['suppress_callback_exceptions'] = True
 server = app.server
+
+
+app.layout = dbc.Container(
+    dbc.Row(
+        dbc.Col(
+            [
+                html.Div('This is the blog of Russell',id='content'),
+                dbc.Button('Change name to uppercase',id='change',n_clicks=0)
+            ]
+        )
+    )
+)
+
+@app.callback(
+    [Output('content','children'),
+     Output('change','children')],
+    [Input('change','n_clicks')]
+)
+def change_content(n):
+    if n%0:
+        name = 'Russell'
+        case = 'lower'
+    else:
+        name = 'RUSSELL'
+        case = 'lower'
+    return 'This is the blog of '+name, 'Change name to case'.format(case)
+
